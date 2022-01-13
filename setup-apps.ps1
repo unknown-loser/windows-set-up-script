3c0a97211355c5ea3446d1234571192b37ec4a11
unknown-loser <romanovdm932@outlook.com>
2022-01-13 13:24:52 +0200

    Update setup-apps.ps1

--- setup-apps.ps1
+++ setup-apps.ps1
@@ -1,1 +1,1 @@
-choco install firefox discord spotify virtualbox vscode cura 7zip etcher rufus everything nordvpn python winaero-tweaker zoom wiztree git mingw freedownloadmanager f.lux geforce-experience speedtest -y
+choco install firefox discord spotify virtualbox vscode cura 7zip etcher rufus everything python winaero-tweaker zoom wiztree git mingw freedownloadmanager f.lux geforce-experience speedtest -y
