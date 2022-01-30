9c32d815cb23cdada084381c65c08e7d3940ba37
unknown-loser <unknown_loser.exe@protonmail.com>
2022-01-30 11:49:51 +0200

    refactored repo

---
+++ README.md
@@ -0,0 +1,3 @@
+Setup script for my PC just to get up and running quickly for a fresh install
+
+adapted from https://github.com/Disassembler0/Win10-Initial-Setup-Script
---
+++ choco-setup-script/setup-apps.ps1
@@ -0,0 +1,1 @@
+choco install firefox discord spotify virtualbox vscode cura 7zip etcher rufus everything python winaero-tweaker zoom wiztree git mingw freedownloadmanager f.lux geforce-experience speedtest -y
